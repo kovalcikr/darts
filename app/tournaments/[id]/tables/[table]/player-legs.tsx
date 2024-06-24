12389f62d3515de0a9132d383afb97b325ab5368
Radovan Kovalcik <radovan.kovalcik@oracle.com>
2024-06-24 14:20:44 +0000

    use smaller font

--- app/tournaments/[id]/tables/[table]/player-legs.tsx
+++ app/tournaments/[id]/tables/[table]/player-legs.tsx
@@ -1,7 +1,7 @@
 export default function PlayerLegs({ last, avg, darts } : { last : number, avg: number, darts: number}) {
   return (
     <div className="flex flex-col basis-1/2">
-      <div className="flex flex-row items-center justify-center text-center p-2 text-2xl">
+      <div className="flex flex-row items-center justify-center text-center p-2 text-1xl">
         <div className="p-3">Last: {last}</div>
         <div className="p-3">Leg Darts: {darts}</div>
         <div className="p-3">Match Avg: {avg}</div>
