a67f844442bd51d630ac69d01cd3f4515186415b
Radovan Kovalcik <radovan.kovalcik@oracle.com>
2024-07-09 17:10:26 +0000

    finish match improvement

--- app/tournaments/[id]/tables/[table]/winner.tsx
+++ app/tournaments/[id]/tables/[table]/winner.tsx
@@ -9,18 +9,18 @@ export default function Winner({ player, image, match, leg, slow }: { player: st
     return (
         <div className="flex flex-col text-center justify-center items-center content-center">
         Winner: 
-        <img src={image} className="w-24 "></img>
+        <img src={image} className="w-20"></img>
   <div className={"flex p-2 text-2xl font-bold"}>
   {player}
   </div>
   <div className="flex flex-col p-1 m-1">  <GamepadButton
         name="Undo"
-        color="bg-orange-600 p-6 border border-2 round flex felx-col"
+        color="bg-orange-600 p-5 border border-2 round flex felx-col"
         onClick={async () => await undoThrow(match.id, leg, slow)}
       /></div>
         <GamepadButton
         name="Finish Match"
-        color="bg-green-600 p-6 border border-2 round flex flex-col"
+        color="bg-green-600 p-5 border border-2 round flex flex-col"
         onClick={async () => await finishMatch(match.tournamentId, match.id, match.playerALegs, match.playerBlegs)}
       />
         </div>
