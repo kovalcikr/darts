e3e44d9e6f6bea33a42513c79cd1a8d19bca8cf5
Radovan Kovalcik <radovan.kovalcik@oracle.com>
2024-07-31 15:51:26 +0000

    auto finish match, swap undo and finish match buttons

--- app/tournaments/[id]/tables/[table]/winner.tsx
+++ app/tournaments/[id]/tables/[table]/winner.tsx
@@ -4,25 +4,39 @@ import { undoThrow } from "@/app/lib/playerThrow";
 import GamepadButton from "./gamepad-button";
 import { finishMatch } from "@/app/lib/cuescore";
 import { Match } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react"
 
 export default function Winner({ player, image, match, leg, slow }: { player: string, image: string, match: Match, leg: number, slow: boolean }) {
-    return (
-        <div className="flex flex-col text-center justify-center items-center content-center">
-        Winner: 
-        <img src={image} className="w-20"></img>
-  <div className={"flex p-2 text-2xl font-bold"}>
-  {player}
-  </div>
-  <div className="flex flex-col p-1 m-1">  <GamepadButton
-        name="Undo"
-        color="bg-orange-600 p-5 border border-2 round flex felx-col"
-        onClick={async () => await undoThrow(match.id, leg, slow)}
-      /></div>
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const comInterval = setInterval(() => {
+      finishMatch(match.tournamentId, match.id, match.playerALegs, match.playerBlegs)
+    }, 20000); //This will refresh the data at regularIntervals of refreshTime
+    return () => clearInterval(comInterval) //Clear interval on component unmount to avoid memory leak
+  }, [])
+
+  return (
+    <div className="flex flex-col text-center justify-center items-center content-center">
+      Winner:
+      <img src={image} className="w-20"></img>
+      <div className={"flex p-2 text-2xl font-bold"}>
+        {player}
+      </div>
+      <div className="flex flex-col p-1 m-1">
         <GamepadButton
-        name="Finish Match"
-        color="bg-green-600 p-5 border border-2 round flex flex-col"
-        onClick={async () => await finishMatch(match.tournamentId, match.id, match.playerALegs, match.playerBlegs)}
-      />
-        </div>
-    );
-  }
+          name="Finish Match"
+          color="bg-green-600 p-5 border border-2 round flex flex-col"
+          onClick={async () => await finishMatch(match.tournamentId, match.id, match.playerALegs, match.playerBlegs)}
+        />
+        <GamepadButton
+          name="Undo"
+          color="bg-orange-600 p-5 border border-2 round flex felx-col"
+          onClick={async () => await undoThrow(match.id, leg, slow)}
+        />
+      </div>
+    </div>
+  );
+}
